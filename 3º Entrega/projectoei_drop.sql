e41ad9972ef3788fdd5e9596b871c92536348e08
rafaelcsants <73482036+rafaelcsants@users.noreply.github.com>
2021-01-07 11:29:39 +0000

    Base de dados

---
+++ 3º Entrega/projectoei_drop.sql
@@ -0,0 +1,11 @@
+#Rafael Santos 20190800
+use projecto_eico2;
+SET FOREIGN_KEY_CHECKS = 0;
+drop table anuncio;
+drop table clientes;
+drop table cparticular;
+drop table empresa;
+drop table funcionario;
+drop table funcionario_has_anuncio;
+drop table cparticular_has_anuncio;
+SET FOREIGN_KEY_CHECKS = 1;
